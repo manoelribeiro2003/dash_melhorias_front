ac1d107b2c2e28e914c05f1c7dba8165dcb3b708
Manoel <ribeiromanoel2003@gmail.com>
2026-09-14 14:59:15 -0300

    feat: search user in projects card

--- src/app/features/projects/components/projects-card/projects-card.ts
+++ src/app/features/projects/components/projects-card/projects-card.ts
@@ -7,6 +7,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { UsuarioService } from '../../../../shared/services/usuario/usuario.service';
 import { categorias } from '../../../../shared/utils/categories';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { Usuario } from '../../../../shared/models/usuario/usuario.interface';
+import { FormsModule } from '@angular/forms';
 
 interface Status {
   value: string;
@@ -22,6 +25,8 @@ interface Status {
     MatButtonToggleModule,
     MatFormFieldModule,
     MatSelectModule,
+    NgxMatSelectSearchModule,
+    FormsModule
   ],
   templateUrl: './projects-card.html',
   styleUrl: './projects-card.scss',
@@ -57,4 +62,24 @@ export class ProjectsCard {
   statusSelecionado = model(null);
   readonly usuarioSelecionado = model<number | null>(null);
   readonly gestorSelecionado = model<number | null>(null);
+
+  // -------------------------Pesquisa de usuario------------------------------
+  protected readonly pesquisaUsuario = signal('');
+  protected readonly usuariosFiltrados = computed(() => {
+    const termo = this.pesquisaUsuario().toLowerCase().trim();
+
+    const resultado = this.usuarios().filter((usuario) =>
+      usuario.nome.toLowerCase().includes(termo),
+    );
+
+    return resultado;
+  });
+  protected alterarPesquisaUsuario(valor: string): void {
+    this.pesquisaUsuario.set(valor);
+  }
+  // ----------------------------------------------------------------------------------
+
+  compararUsuarios(usuario1: Usuario | null, usuario2: Usuario | null): boolean {
+    return usuario1?.id === usuario2?.id;
+  }
 }
